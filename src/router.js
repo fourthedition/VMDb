843ce2a218c3bb36572c204131b8df2721129fd9
Fourth <sirawit@dek-d.com>
2021-08-05 18:39:02 +0700

    Update

--- src/router.js
+++ src/router.js
@@ -6,16 +6,16 @@ import {
 import Home from '@/pages/Home.vue'
 
 const routes = [{
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/detail/:id',
-    name: 'detail',
-    component: () => import('@/pages/Detail.vue'),
-    props: true
-  }
+  path: '/',
+  name: 'home',
+  component: Home
+},
+{
+  path: '/detail/:id',
+  name: 'detail',
+  component: () => import('@/pages/Detail.vue'),
+  props: true
+}
 ]
 
 const router = createRouter({
